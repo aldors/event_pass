b2be4d34ab95200b5334a28541978e75afc527dc
Aldo Ruiz <minialdocr@gmail.com>
2026-09-04 23:01:38 -0600

    initial commit

---
+++ src/main/java/com/aldo/event_pass/EventPassApplication.java
@@ -0,0 +1,13 @@
+package com.aldo.event_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPassApplication.class, args);
+	}
+
+}
