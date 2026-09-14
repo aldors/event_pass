2aa589bb5f204dc7b505c5c982dfc6c23c2cfc05
Aldo Ruiz <minialdocr@gmail.com>
2026-09-14 12:50:25 -0600

    feat: crear EventoPublicController para usuarios

---
+++ src/main/java/com/aldo/event_pass/controller/EventoPublicController.java
@@ -0,0 +1,34 @@
+package com.aldo.event_pass.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aldo.event_pass.dto.evento.EventoDetalleResponse;
+import com.aldo.event_pass.dto.evento.EventoListadoResponse;
+import com.aldo.event_pass.service.interfaces.EventoPublicService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/eventos")
+@RequiredArgsConstructor
+public class EventoPublicController {
+
+    private final EventoPublicService eventoPublicService;
+
+    @GetMapping("/obtener")
+    public ResponseEntity<List<EventoListadoResponse>> obtenerEventos(){
+        return ResponseEntity.ok(eventoPublicService.obtenerEventos());
+    }
+
+    @GetMapping("/{eventoId}/obtener")
+    public ResponseEntity<EventoDetalleResponse> obtenerEventosPorId(@PathVariable Long eventoId){
+        return ResponseEntity.ok(eventoPublicService.obtenerEventosPorId(eventoId));
+    }
+    
+}
