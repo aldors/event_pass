a31ac2677835a14a2f128863f1f64ecb64a3079d
Aldo Ruiz <minialdocr@gmail.com>
2026-09-16 14:11:49 -0600

    feat: agregar nuevos endpoints protegidos para admin

--- src/main/java/com/aldo/event_pass/security/securityConfig.java
+++ src/main/java/com/aldo/event_pass/security/securityConfig.java
@@ -32,6 +32,7 @@ public class securityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/registro", "/auth/login").permitAll()
                         .requestMatchers("/auth/refresh-token", "/auth/logout", "/auth/me").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/admin/eventos/crear", "/admin/eventos/{eventoId}/agregar-tipo-boleto", "/admin/eventos/{eventoId}/publicar").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
