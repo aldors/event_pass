f0145b78ebd2913bac3cec4a5a3287fe0712c6f0
Aldo Ruiz <minialdocr@gmail.com>
2026-09-21 17:08:54 -0600

    feat: agregar nuevo endpoint en BoletoController para generar el pdf

--- src/main/java/com/aldo/event_pass/controller/BoletoController.java
+++ src/main/java/com/aldo/event_pass/controller/BoletoController.java
@@ -1,5 +1,7 @@
 package com.aldo.event_pass.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,4 +32,18 @@ public class BoletoController {
         return ResponseEntity.ok(boletoService.usarBoleto(codigoQr));
     }
 
+    @GetMapping("/{id}/pdf")
+    public ResponseEntity<byte[]> generarPdf(@PathVariable Long id) {
+
+        byte[] pdf = boletoService.generarPdf(id);
+
+        return ResponseEntity.ok().header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=boleto-" + id + ".pdf"
+                )
+                .contentType(
+                        MediaType.APPLICATION_PDF
+                )
+                .body(pdf);
+    }
 }
