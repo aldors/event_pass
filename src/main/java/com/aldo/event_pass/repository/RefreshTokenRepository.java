a4aacaecf54f945257c9c4c2b62c15652dd35924
Aldo Ruiz <minialdocr@gmail.com>
2026-09-23 16:04:47 -0600

    feat: agregar nuevo método para eliminar tokens deleteByUsuario

--- src/main/java/com/aldo/event_pass/repository/RefreshTokenRepository.java
+++ src/main/java/com/aldo/event_pass/repository/RefreshTokenRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.aldo.event_pass.entity.RefreshToken;
+import com.aldo.event_pass.entity.Usuario;
 
 @Repository 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     
     Optional<RefreshToken> findByToken(String refreshToken);
     void deleteByToken(String refreshToken);
+    void deleteByUsuario(Usuario usuario);
 }
