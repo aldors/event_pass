4a15c28bc0d320a9c88196df4052e8c0283eaafc
Aldo Ruiz <minialdocr@gmail.com>
2026-09-23 17:14:13 -0600

    feat: manejar concurrencia para evitar que dos hilos paguen una compra al mismo tiempo

--- src/main/java/com/aldo/event_pass/repository/CompraRepository.java
+++ src/main/java/com/aldo/event_pass/repository/CompraRepository.java
@@ -3,10 +3,14 @@ package com.aldo.event_pass.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import com.aldo.event_pass.entity.Compra;
 
+import jakarta.persistence.LockModeType;
+
 public interface CompraRepository extends JpaRepository<Compra, Long> {
     
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Compra> findByIdAndUsuarioId(Long compraId, Long usuarioId);
 }
