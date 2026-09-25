4ad72f988b338c8444894ba5bab032f186b7c1d4
Aldo Ruiz <minialdocr@gmail.com>
2026-09-25 17:03:13 -0600

    fix: ajustar longitud de refresh token a 255 en la entidad RefreshToken por limites de indice en MySQL

--- src/main/java/com/aldo/event_pass/entity/RefreshToken.java
+++ src/main/java/com/aldo/event_pass/entity/RefreshToken.java
@@ -22,7 +22,7 @@ public class RefreshToken {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 1000)
+    @Column(nullable = false, unique = true, length = 255)
     private String token;
 
     @ManyToOne
